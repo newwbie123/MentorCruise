2189789a475a07704929cd93b3149ad674517f62
Ivan Tadic <ivantadic3@gmail.com>
2019-01-25 19:54:55 +0100

    tried to write percentage calculator with user input limited to numbers, but stucked

---
+++ src/percentageCalculator/PercentageCalculator.java
@@ -0,0 +1,43 @@
+package percentageCalculator;
+
+import java.util.Scanner;
+
+public class PercentageCalculator {
+
+	public static void main(String[] args) {
+
+		double[] grades = new double[5];
+		Scanner scanner = new Scanner(System.in);
+		int examPoints = 0;
+		int studentNo = 1;
+
+		try {
+
+			for (int i = 0; i < grades.length; i++) {
+				System.out.println("Please enter achieved exam points for the Student no. " + studentNo);
+
+				while (scanner.hasNextInt()) {
+					examPoints = scanner.nextInt();
+					double examMaxPoints = 150.0;
+					double percentageGrade = (examPoints / examMaxPoints) * 100;
+
+					grades[i] = percentageGrade;
+					studentNo++;
+
+					System.out.println("The percentage for Student " + studentNo + " is " + percentageGrade);
+					
+
+				}
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		
+		}
+
+		scanner.close();
+
+	}
+
+}
